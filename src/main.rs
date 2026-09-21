154527e6c47f3127eadff83f20f86a5626949202
Rafael Requião <rafaelfrequiao@gmail.com>
2026-09-21 16:53:43 -0300

    fix(main): remove as importacoes duplicadas que a edicao mecanica deixou

--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,6 @@ use zeebx::{audio, cpu, input, library, loader, machine, session, ui};
 
 use std::process::ExitCode;
 
-use zeebx::{audio, cpu, input, library, loader, machine, session, ui};
-
-use std::process::ExitCode;
-
 use zeebx::brew::aee;
 use zeebx::cpu::{BackendPadrao, CpuBackend, dynarmic::DynarmicCpu};
 use zeebx::input::bindings;
